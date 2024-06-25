0627c82dbbeb842196687d34961da1e7a5822f35
shriya1808 <64626036+shriya1808@users.noreply.github.com>
2024-06-26 00:07:13 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Uncertainty Estimation',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting the Severity of Support Tickets from Machine Event Data',
+    author='Shriya Mittapalli',
+    author_email='shriya.mittapalli@fau.de',
+    license='',
+)
